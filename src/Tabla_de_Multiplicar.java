3a04be842daf3f549d5627a4bbd66cd4baf486c7
xianDT <xiandt01@gmail.com>
2023-12-29 17:00:13 +0100

    Segundo commit

--- src/Tabla_de_Multiplicar.java
+++ src/Tabla_de_Multiplicar.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Tabla_de_Multiplicar {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce un número para mostrar su tabla de multiplicar:");
+        int numero = entrada.nextInt();
 
+        System.out.println("Tabla de multiplicar del número " + numero + ":");
 
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        entrada.close();
     }
 }
